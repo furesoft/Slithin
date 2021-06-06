dc76176a8abfeda1591a1d8026458af3d4168a23
Chris <filmee24@gmail.com>
2021-06-06 10:09:21 +0200

    Add PathList

---
+++ Source/Core/PathList.cs
@@ -0,0 +1,9 @@
+namespace Slithin.Core
+{
+    public static class PathList
+    {
+        public const string Documents = ".local/share/remarkable/xochitl";
+        public const string Templates = "/usr/share/remarkable/templates/";
+        public const string Images = "/usr/share/remarkable/";
+    }
+}
