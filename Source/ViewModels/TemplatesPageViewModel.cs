b2ef7b8e301c5e260f87640b5b67252ff350315f
Chris <filmee24@gmail.com>
2021-06-08 16:59:12 +0200

    Add Searchbar and filled Categories Combobx

--- Source/ViewModels/TemplatesPageViewModel.cs
+++ Source/ViewModels/TemplatesPageViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Input;
 using Slithin.Core;
 using Slithin.Core.Remarkable;
@@ -13,10 +14,13 @@ namespace Slithin.ViewModels
 
         public ICommand RefreshCommand { get; set; }
 
+        public ObservableCollection<string> Categories { get; set; }
+
         public TemplatesPageViewModel()
         {
             RefreshCommand = new DelegateCommand(Refresh);
             Templates = new();
+            Categories = new();
         }
 
         private void Refresh(object? obj)
@@ -25,10 +29,24 @@ namespace Slithin.ViewModels
 
             TemplateStorage.Instance?.Load();
 
+            var tempCats = TemplateStorage.Instance?.Templates.Select(_ => _.Categories);
+            foreach (var item in tempCats)
+            {
+                foreach (var cat in item)
+                {
+                    if (!Categories.Contains(cat))
+                    {
+                        Categories.Add(cat);
+                    }
+                }
+            }
+
             foreach (var item in TemplateStorage.Instance?.Templates)
             {
                 Templates.Add(item);
             }
+
+
         }
     }
 }
