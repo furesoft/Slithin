e30e495e779f9fa66f0df735109c3635877e4d33
Chris <filmee24@gmail.com>
2021-06-10 16:29:38 +0200

    Add Dialog controls

---
+++ Source/Controls/DialogControl.cs
@@ -0,0 +1,76 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Styling;
+using Slithin.Core;
+using System;
+using System.Windows.Input;
+
+namespace Slithin.Controls
+{
+    public class DialogControl : ContentControl, IStyleable
+    {
+        public static StyledProperty<ICommand> CancelCommandProperty =
+            AvaloniaProperty.Register<DialogControl, ICommand>(nameof(CancelCommand));
+
+        public static StyledProperty<ICommand> CommandProperty =
+            AvaloniaProperty.Register<DialogControl, ICommand>(nameof(Command));
+
+        public static StyledProperty<string> CommandTextProperty =
+            AvaloniaProperty.Register<DialogControl, string>(nameof(CommandText));
+
+        public static StyledProperty<string> HeaderProperty =
+                    AvaloniaProperty.Register<DialogControl, string>(nameof(Header));
+
+        public static StyledProperty<bool> IsCancelEnabledProperty =
+                                            AvaloniaProperty.Register<DialogControl, bool>(nameof(IsCancelEnabled));
+
+        public DialogControl()
+        {
+            CancelCommand = new DelegateCommand((o) => DialogService.Close());
+        }
+
+        public ICommand CancelCommand
+        {
+            get { return GetValue<ICommand>(CancelCommandProperty); }
+            set
+            {
+                SetValue(CancelCommandProperty, value);
+            }
+        }
+
+        public ICommand Command
+        {
+            get { return GetValue<ICommand>(CommandProperty); }
+            set
+            {
+                SetValue(CommandProperty, value);
+            }
+        }
+
+        public string CommandText
+        {
+            get { return GetValue<string>(CommandTextProperty); }
+            set
+            {
+                SetValue(CommandTextProperty, value);
+            }
+        }
+
+        public string Header
+        {
+            get { return GetValue<string>(HeaderProperty); }
+            set
+            {
+                SetValue(HeaderProperty, value);
+            }
+        }
+
+        public bool IsCancelEnabled
+        {
+            get { return GetValue<bool>(IsCancelEnabledProperty); }
+            set { SetValue(IsCancelEnabledProperty, value); }
+        }
+
+        Type IStyleable.StyleKey => typeof(DialogControl);
+    }
+}
---
+++ Source/Controls/DialogService.cs
@@ -0,0 +1,64 @@
+﻿using Avalonia.Controls;
+using Slithin.Core;
+using System.Windows.Input;
+
+namespace Slithin.Controls
+{
+    public static class DialogService
+    {
+        private static ContentDialog _host;
+
+        public static void Close()
+        {
+            if (_host != null)
+            {
+                _host.IsOpened = false;
+            }
+        }
+
+        public static ICommand CreateOpenCommand(Control content, BaseViewModel viewModel)
+        {
+            return new DelegateCommand((o) =>
+            {
+                Open(content, viewModel);
+            });
+        }
+
+        public static bool GetIsHost(ContentDialog target)
+        {
+            return object.ReferenceEquals(_host, target);
+        }
+
+        public static void Open(object content)
+        {
+            if (_host != null)
+            {
+                _host.DialogContent = content;
+                _host.IsOpened = true;
+            }
+        }
+
+        public static void Open(Control content, BaseViewModel viewModel)
+        {
+            content.DataContext = viewModel;
+
+            Open(content);
+        }
+
+        public static void Open()
+        {
+            if (_host != null)
+            {
+                _host.IsOpened = true;
+            }
+        }
+
+        public static void SetIsHost(ContentDialog target, bool value)
+        {
+            if (value)
+            {
+                _host = target;
+            }
+        }
+    }
+}
