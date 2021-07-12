4531237fdc528374f453f539148e422d56974dc6
filmee24 <filmee24@gmail.com>
2021-07-12 16:32:38 +0200

    Cleanup

--- Source/ViewModels/NotebooksPageViewModel.cs
+++ Source/ViewModels/NotebooksPageViewModel.cs
@@ -1,9 +1,27 @@
-﻿using Slithin.Core;
+﻿using System.Windows.Input;
+using Slithin.Core;
+using Slithin.Core.Remarkable;
 
 namespace Slithin.ViewModels
 {
     public class NotebooksPageViewModel : BaseViewModel
     {
+        private Metadata _selectedNotebook;
 
+        public NotebooksPageViewModel()
+        {
+            // OpenAddModalCommand = DialogService.CreateOpenCommand<AddNotebookModal>(new AddTemplateModalViewModel());
+            // RemoveTemplateCommand = new RemoveNotebookCommand(this);
+        }
+
+        public ICommand OpenAddModalCommand { get; set; }
+
+        public ICommand RemoveTemplateCommand { get; set; }
+
+        public Metadata SelectedNotebook
+        {
+            get { return _selectedNotebook; }
+            set { SetValue(ref _selectedNotebook, value); }
+        }
     }
 }
