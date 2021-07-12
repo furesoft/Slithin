42284740f3bdc7778e8c7500b94dc7a33cdf0d7f
filmee24 <filmee24@gmail.com>
2021-07-12 17:07:54 +0200

    Add Script Evaluator

---
+++ Source/Core/Automation.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Linq;
+using NiL.JS;
+using NiL.JS.Core;
+
+namespace Slithin.Core
+{
+    public static class Automation
+    {
+        public static Context CreateContext()
+        {
+            var c = new Context();
+
+            c.DefineVariable("lib").Assign(new NamespaceProvider("System"));
+
+            return c;
+        }
+
+        public static void Evaluate(string scriptname, Context context)
+        {
+            context.Eval(File.ReadAllText(Path.Combine(ServiceLocator.ConfigBaseDir, "Scripts", scriptname + ".js")));
+        }
+
+        public static string[] GetScriptNames()
+        {
+            return Directory.GetFiles(
+                    Path.Combine(ServiceLocator.ConfigBaseDir, "Scripts")).
+                    Select(_ => Path.GetFileNameWithoutExtension(_)).
+                    ToArray();
+        }
+    }
+}
