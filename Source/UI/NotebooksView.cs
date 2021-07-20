93b41dd198a6a486fc0b6117bfe52f7588e375c5
filmee24 <filmee24@gmail.com>
2021-07-20 10:21:17 +0200

    Add Folder Mechanics

---
+++ Source/UI/NotebooksView.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Slithin.Core.Remarkable;
+using Slithin.ViewModels;
+
+namespace Slithin.UI
+{
+    public static class NotebooksView
+    {
+        private static ListBox _lb;
+
+        public static bool GetIsView(Control control)
+        {
+            return control == _lb;
+        }
+
+        public static void SetIsView(ListBox lb, bool value)
+        {
+            _lb = lb;
+
+            _lb.DoubleTapped += _lb_DoubleTapped;
+        }
+
+        private static void _lb_DoubleTapped(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            if (_lb.SelectedItem is Metadata md && md.Type == MetadataType.CollectionType)
+            {
+                if (_lb.DataContext is NotebooksPageViewModel vm)
+                {
+                    //ToDo: change to foldermanager
+                    vm.SyncService.NotebooksFilter.Documents.Clear();
+
+                    if (md.VisibleName.Equals("Up .."))
+                    {
+                    }
+                    else
+                    {
+                        vm.SyncService.NotebooksFilter.Documents.Add(new Metadata { Type = MetadataType.DocumentType, VisibleName = "tst doc" });
+                        vm.SyncService.NotebooksFilter.Documents.Add(new Metadata { Type = MetadataType.CollectionType, VisibleName = "Up .." });
+                    }
+
+                    vm.SyncService.NotebooksFilter.SortByFolder();
+                }
+            }
+        }
+    }
+}
