bb09deaa98d1e9f97ec70b65945286d66b047d51
filmee24 <filmee24@gmail.com>
2021-07-27 19:12:26 +0200

    Cleanup

--- Source/Core/Sync/SynchronisationService.cs
+++ Source/Core/Sync/SynchronisationService.cs
@@ -84,7 +84,7 @@ namespace Slithin.Core.Sync
 
         private void Synchronize(object? obj)
         {
-            if (!ServiceLocator.Local.GetTemplates().Any() || !Directory.GetFiles(ServiceLocator.TemplatesDir).Any())
+            if (!ServiceLocator.Local.GetTemplates().Any() && !Directory.GetFiles(ServiceLocator.TemplatesDir).Any())
             {
                 ServiceLocator.Mailbox.Post(new InitStorageMessage());
             }
