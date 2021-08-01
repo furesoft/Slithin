6e2934030e0e2dc0aa09b1d83f7279c92e4d1625
filmee24 <filmee24@gmail.com>
2021-08-01 12:27:29 +0200

    Add ITool Interface

---
+++ Source/Core/ITool.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+
+namespace Slithin.Core
+{
+    public interface ITool
+    {
+        ScriptInfo Info { get; }
+
+        Control GetModal();
+
+        void Invoke(object data);
+    }
+}
