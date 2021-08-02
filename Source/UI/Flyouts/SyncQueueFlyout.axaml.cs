fa42ce6dd56af24f2f2cd96a4916cfb36829e858
filmee24 <filmee24@gmail.com>
2021-08-02 19:13:03 +0200

    Add SyncQueueFlyout

---
+++ Source/UI/Flyouts/SyncQueueFlyout.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Slithin.ViewModels;
+
+namespace Slithin.UI.Flyouts
+{
+    public partial class SyncQueueFlyout : UserControl
+    {
+        public SyncQueueFlyout()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+
+            DataContext = new SyncQueueFlyoutViewModel();
+        }
+    }
+}
---
+++ Source/ViewModels/SyncQueueFlyoutViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Slithin.Core;
+using Slithin.Core.Sync;
+
+namespace Slithin.ViewModels
+{
+    public class SyncQueueFlyoutViewModel : BaseViewModel
+    {
+        private SyncItem _selectedItem;
+
+        public SyncQueueFlyoutViewModel()
+        {
+            Items.Add(new SyncItem { Action = SyncAction.Add, Direction = SyncDirection.ToDevice, Type = SyncType.Template, Data = "Something" });
+            Items.Add(new SyncItem { Action = SyncAction.Add, Direction = SyncDirection.ToDevice, Type = SyncType.Template, Data = "Something" });
+
+            DeleteCommand = new DelegateCommand(Delete);
+        }
+
+        public ICommand DeleteCommand { get; set; }
+
+        public ObservableCollection<SyncItem> Items { get; set; } = new();
+
+        public SyncItem SelectedItem
+        {
+            get { return _selectedItem; }
+            set { SetValue(ref _selectedItem, value); }
+        }
+
+        private void Delete(object obj)
+        {
+            Items.Remove(SelectedItem);
+        }
+    }
+}
