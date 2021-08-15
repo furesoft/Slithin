d08715933ce99e0eb627b8e11fd746913993bb88
filmee24 <filmee24@gmail.com>
2021-08-15 10:10:30 +0200

    Rewrite SvgExporter

--- Source/Core/Remarkable/LinesAreBeatiful/Exporters/SvgExporter.cs
+++ Source/Core/Remarkable/LinesAreBeatiful/Exporters/SvgExporter.cs
@@ -1,224 +1,79 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Text;
+using Slithin.Core.Services;
+using Svg;
+using Svg.Pathing;
 
 namespace Slithin.Core.Remarkable.LinesAreBeatiful.Exporters
 {
     public static class SvgExporter
     {
-        public static string RenderPage(Page page)
+        public static string RenderPage(Page page, int index, Metadata md)
         {
-            StringBuilder sb = new();
-            // SVG header
-            sb.AppendLine("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?>");
-            sb.AppendLine("<svg width=\"1404\" height=\"1872\" viewBox=\"0 0 1404 1872\" ");
-            sb.Append("version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" ");
-            sb.Append("xmlns:xlink=\"http://www.w3.org/1999/xlink\">");
-
-            for (var i = 0; i < page.Layers.Count; i++)
+            var svgDoc = new SvgDocument
             {
-                var layer = page.Layers[i];
+                Width = 1404,
+                Height = 1872,
+                ViewBox = new SvgViewBox(0, 0, 1404, 1872),
+            };
 
-                RenderLayer(i, layer, sb);
-            }
+            var group = new SvgGroup();
+            svgDoc.Children.Add(group);
 
-            // SVG footer
-            sb.AppendLine("</svg>");
+            group.Children.Add(new SvgImage { Href = "data:image/png;base64," + GetBase64Template(index, md), X = 0, Y = 0 });
 
-            return sb.ToString();
-        }
-
-        private static void RenderLayer(int layer_id, Layer layer, StringBuilder sb)
-        {
-            var layer_id_str = "layer-" + layer_id;
-            Stack<RenderGroup> open_groups = new();
-            RenderGroup current = new();
-
-            foreach (var line in layer.Lines)
+            foreach (var layer in page.Layers)
             {
-                switch (line.BrushType)
+                foreach (var line in layer.Lines)
                 {
-                    case Brushes.Rubber:
-                    case Brushes.Rubberarea:
-                        if (current.Strokes.Any())
-                        {
-                            // Register an erasure only if there exists previous
-                            // strokes to erase (otherwise, there is nothing to
-                            // erase and it can be safely ignored).
-                            current.Erasures.Add(line);
-                        }
-
-                        break;
-
-                    default:
-                        if (current.Erasures.Any())
-                        {
-                            // We cannot add a new stroke to a group that already contains
-                            // some erasures, because this new stroke should not be
-                            // affected by the previous erasures; so, we create a new group.
-                            open_groups.Push(current);
-                            current = new();
-                        }
-
-                        current.Strokes.Add(line);
-                        break;
+                    RenderLine(line, group);
                 }
             }
 
-            open_groups.Push(current);
+            var stream = new MemoryStream();
+            svgDoc.Write(stream);
 
-            // (Second pass.) Pop out groups from the stack, thereby creating
-            // masks and opening the SVG groups. Prepare a reversed stack to
-            // later close the groups.
-            Stack<RenderGroup> close_groups = new();
-            uint mask_id = 0u;
-
-            while (open_groups.Any())
-            {
-                var group = open_groups.Pop();
-
-                // Create a mask group for all erasures of the group, if any
-                if (group.Erasures.Any())
-                {
-                    var mask_id_str = layer_id_str + "-mask-" + mask_id;
-
-                    sb.AppendLine("\t<mask id=\"" + mask_id_str + "\">");
-                    sb.Append("\t\t<rect width=\"100%\" height=\"100%\" fill=\"white\" />");
-
-                    foreach (var erasure in group.Erasures)
-                    {
-                        RenderLine(erasure, sb);
-                    }
-
-                    sb.AppendLine("\t</mask>");
-                    sb.AppendLine("\t<g mask=\"url(#" + mask_id_str + ")\" ");
-                }
-                else
-                {
-                    sb.AppendLine("\t<g ");
-                }
-
-                if (mask_id == 0)
-                {
-                    sb.Append("id=\"" + layer_id_str + "\" ");
-                }
-
-                sb.Append(">");
-
-                close_groups.Push(group);
-
-                ++mask_id;
-            }
-
-            // (Third pass.) Generate paths for each stroke of each group and
-            // then close the groups.
-            while (close_groups.Any())
-            {
-                var cgroup = close_groups.Pop();
-                foreach (var stroke in cgroup.Strokes)
-                {
-                    RenderLine(stroke, sb);
-                }
-
-                sb.AppendLine("\t</g>");
-            }
+            return Encoding.UTF8.GetString(stream.GetBuffer());
         }
 
-        private static void RenderLine(Line line, StringBuilder sb)
+        private static SvgPathSegmentList GeneratePathData(List<Point> points)
         {
-            switch (line.BrushType)
-            {
-                case Brushes.Rubberarea:
-                    RenderRubberArea(line, sb);
-                    break;
+            var psl = new SvgPathSegmentList();
+            psl.Add(new SvgMoveToSegment(new PointF(points[0].X, points[0].Y)));
 
-                default:
-                    RenderNormalLine(line, sb);
-                    break;
+            for (int i = 0; i + 1 < points.Count; i++)
+            {
+                psl.Add(new SvgLineSegment(
+                    new PointF(points[i].X, points[i].Y),
+                    new PointF(points[i + 1].X, points[i + 1].Y))
+                );
+
+                i++;
             }
+            psl.Add(new SvgClosePathSegment());
+
+            return psl;
         }
 
-        private static void RenderNormalLine(Line line, StringBuilder sb)
+        private static string GetBase64Template(int i, Metadata md)
         {
-            // TODO: apply brush texture, pressure and tilt parameters
+            var filename = md.PageData.Data[i];
+            var pathManager = ServiceLocator.Container.Resolve<IPathManager>();
+            var buffer = File.ReadAllBytes(Path.Combine(pathManager.TemplatesDir, filename + ".png"));
 
-            sb.AppendLine("\t<path ");
-            sb.Append("fill=\"none\" ");
-            sb.Append("stroke=\"");
-
-            switch (line.Color)
-            {
-                case Colors.Grey:
-                    sb.Append("grey");
-                    break;
-
-                case Colors.White:
-                    sb.Append("white");
-                    break;
-
-                default:
-                    sb.Append("black");
-                    break;
-            }
-
-            sb.Append("\" ");
-            sb.Append("stroke-width=\"");
-
-            switch (line.BrushType)
-            {
-                case Brushes.Highlighter:
-                case Brushes.Rubber:
-                    sb.Append(20 * BaseSizes.GetValue(line.BrushBaseSize));
-                    break;
-
-                default:
-                    sb.Append(18 * BaseSizes.GetValue(line.BrushBaseSize) - 32);
-                    break;
-            }
-
-            sb.Append("\" ");
-
-            sb.Append("d=\"");
-            RenderPathData(line.Points, sb);
-            sb.Append("\" ");
-
-            if (line.BrushType == Brushes.Highlighter)
-            {
-                sb.Append("opacity=\"0.25\" ");
-            }
-
-            sb.Append("stroke-linejoin=\"round\" ");
-            sb.Append("stroke-linecap=\"round\" ");
-            sb.Append("/>");
+            return Convert.ToBase64String(buffer);
         }
 
-        private static void RenderPathData(List<Point> points, StringBuilder sb)
+        private static void RenderLine(Line line, SvgGroup group)
         {
-            bool is_first = true;
+            var path = new SvgPath();
 
-            foreach (var point in points)
-            {
-                sb.Append(is_first ? "M" : "L");
-                sb.Append(point.X + "," + point.Y);
+            path.PathData = GeneratePathData(line.Points);
 
-                is_first = false;
-            }
-        }
-
-        private static void RenderRubberArea(Line line, StringBuilder sb)
-        {
-            sb.AppendLine("\t<path ");
-            sb.Append("fill=\"black\" ");
-            sb.Append("stroke=\"none\" ");
-            sb.Append("d=\"");
-            RenderPathData(line.Points, sb);
-            sb.Append("\" />");
-        }
-
-        private class RenderGroup
-        {
-            public List<Line> Erasures = new();
-            public List<Line> Strokes = new();
+            group.Children.Add(path);
         }
     }
 }
