b4f20661f0449859a343b7367b01496656baaa52
filmee24 <filmee24@gmail.com>
2021-08-15 14:13:08 +0200

    Fix PageRange

---
+++ Source/Core/Remarkable/Exporting/Rendering/ExportOptions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Slithin.Core.Remarkable.Rendering
+{
+    public class ExportOptions
+    {
+        private List<int> PagesIndices { get; set; } = new(); //int is index for page
+    }
+}
