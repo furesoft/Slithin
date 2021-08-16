2c0e40aead42dd6d677dffdcc3463200ea95ac6b
filmee24 <filmee24@gmail.com>
2021-08-16 13:50:14 +0200

    Implement StartCommand

---
+++ Source/ViewModels/GalleryWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+using Slithin.Core;
+
+namespace Slithin.ViewModels
+{
+    public class GalleryWindowViewModel : BaseViewModel
+    {
+        public GalleryWindowViewModel()
+        {
+            StartCommand = new DelegateCommand(_ =>
+            {
+                RequestClose();
+            });
+        }
+
+        public ICommand StartCommand { get; set; }
+    }
+}
