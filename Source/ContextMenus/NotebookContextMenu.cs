adf828792082b3f4d15ec7d1b4c4de302e16e907
filmee24 <filmee24@gmail.com>
2021-08-30 17:34:04 +0200

    Add Copy ID Command

--- Source/ContextMenus/NotebookContextMenu.cs
+++ Source/ContextMenus/NotebookContextMenu.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Avalonia.Controls;
 using Slithin.Core.ItemContext;
 using Slithin.Core.Remarkable;
-using Slithin;
 using Slithin.Core;
 using Slithin.ViewModels.Pages;
+using System.Windows;
 
 namespace Slithin.ContextMenus
 {
@@ -26,7 +22,7 @@ namespace Slithin.ContextMenus
         {
             if (ParentViewModel is NotebooksPageViewModel n)
             {
-                yield return new MenuItem { Header = "Copy ID" }; //ToDo: implement copy id
+                yield return new MenuItem { Header = "Copy ID", Command = new DelegateCommand(async _ => await App.Current.Clipboard.SetTextAsync(((Metadata)obj).ID)) };
                 yield return new MenuItem { Header = "Remove", Command = new DelegateCommand(_ => n.RemoveNotebookCommand.Execute(obj)) };
                 yield return new MenuItem { Header = "Rename", Command = new DelegateCommand(_ => n.RenameCommand.Execute(obj)) };
             }
