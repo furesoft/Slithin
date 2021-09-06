d73e87ae12dd5c0d89f3c13c8f63ce59331386e9
filmee24 <filmee24@gmail.com>
2021-09-06 10:53:20 +0200

    Add StoreCardCollection Control

---
+++ Source/Controls/StoreCardCollection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Controls.Primitives;
+using Slithin.Models;
+
+namespace Slithin.Controls
+{
+    public class StoreCardCollection : TemplatedControl
+    {
+        public static StyledProperty<ObservableCollection<Sharable>> CardsProperty = AvaloniaProperty.Register<StoreCardCollection, ObservableCollection<Sharable>>("Cards");
+        public static StyledProperty<string> CategoryProperty = AvaloniaProperty.Register<StoreCardCollection, string>("Category");
+        public static StyledProperty<Sharable> SelectedCardProperty = AvaloniaProperty.Register<StoreCardCollection, Sharable>("SelectedCard");
+
+        public ObservableCollection<Sharable> Cards
+        {
+            get { return GetValue(CardsProperty); }
+            set { SetValue(CardsProperty, value); }
+        }
+
+        public string Category
+        {
+            get { return GetValue(CategoryProperty); }
+            set { SetValue(CategoryProperty, value); }
+        }
+
+        public Sharable SelectedCard
+        {
+            get { return GetValue(SelectedCardProperty); }
+            set { SetValue(SelectedCardProperty, value); }
+        }
+    }
+}
