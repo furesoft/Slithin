d0c09c66838bdb2c8e0d81dfeaa323c5c2e1a728
filmee24 <filmee24@gmail.com>
2021-09-06 08:32:08 +0200

    Add Better Notebook Download Message

---
+++ Source/Messages/DownloadSyncNotebookMessage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Slithin.Core;
+using Slithin.Models;
+
+namespace Slithin.Messages
+{
+    public class DownloadSyncNotebookMessage : AsynchronousMessage
+    {
+        public DownloadSyncNotebookMessage(List<SyncNotebook> notebooks)
+        {
+            Notebooks = notebooks;
+        }
+
+        public List<SyncNotebook> Notebooks { get; set; }
+    }
+}
