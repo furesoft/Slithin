37d1b48088e1f00e790772340ea5b56c8c5dc0ee
filmee24 <filmee24@gmail.com>
2021-09-06 10:53:09 +0200

    Cleanup

--- Source/ViewModels/Pages/SharablesPageViewModel.cs
+++ Source/ViewModels/Pages/SharablesPageViewModel.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 using Slithin.Core;
 using Slithin.Models;
 
@@ -8,12 +13,21 @@ namespace Slithin.ViewModels.Pages
     {
         public SharablesPageViewModel()
         {
-            Items.Add(new() { ID = "1", IsInstalled = false, Name = "Not Installed Template 1" });
-            Items.Add(new() { ID = "2", IsInstalled = true, Name = "Installed Template 2" });
-            Items.Add(new() { ID = "3", IsInstalled = false, Name = "Not Installed Template 3" });
-            Items.Add(new() { ID = "4", IsInstalled = true, Name = "Installed Template 4" });
+            Items.Add(new() { ID = "1", IsInstalled = false, Name = "Not Installed Template 1", Image = LoadImage("backup"), Author = "Furesoft" });
+            Items.Add(new() { ID = "2", IsInstalled = true, Name = "Installed Template 2", Image = LoadImage("epub"), Author = "Furesoft" });
+            Items.Add(new() { ID = "3", IsInstalled = false, Name = "Not Installed Template 3", Image = LoadImage("folder"), Author = "Furesoft" });
+            Items.Add(new() { ID = "4", IsInstalled = true, Name = "Installed Template 4", Image = LoadImage("pdf"), Author = "Furesoft" });
+            Items.Add(new() { ID = "5", IsInstalled = true, Name = "Installed Template 5", Image = LoadImage("folder"), Author = "Furesoft" });
+            Items.Add(new() { ID = "6", IsInstalled = false, Name = "Not Installed Template 5", Image = LoadImage("backup"), Author = "Furesoft" });
         }
 
         public ObservableCollection<Sharable> Items { get; set; } = new();
+
+        private IImage LoadImage(string name)
+        {
+            var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
+
+            return new Bitmap(assets.Open(new Uri($"avares://Slithin/Resources/{name}.png")));
+        }
     }
 }
