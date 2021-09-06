d2c591d261b63ee0ec47283abc3b750394c59fdd
filmee24 <filmee24@gmail.com>
2021-09-06 13:09:08 +0200

    Cleanup

--- Source/ViewModels/Pages/SharablesPageViewModel.cs
+++ Source/ViewModels/Pages/SharablesPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using Avalonia;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
@@ -19,9 +20,16 @@ namespace Slithin.ViewModels.Pages
             Items.Add(new() { ID = "4", IsInstalled = true, Name = "Installed Template 4", Image = LoadImage("pdf"), Author = "Furesoft" });
             Items.Add(new() { ID = "5", IsInstalled = true, Name = "Installed Template 5", Image = LoadImage("folder"), Author = "Furesoft" });
             Items.Add(new() { ID = "6", IsInstalled = false, Name = "Not Installed Template 5", Image = LoadImage("backup"), Author = "Furesoft" });
+
+            ViewMoreCommand = new DelegateCommand(_ =>
+            {
+                NotificationService.Show(_.ToString());
+            });
         }
 
         public ObservableCollection<Sharable> Items { get; set; } = new();
+
+        public ICommand ViewMoreCommand { get; set; }
 
         private IImage LoadImage(string name)
         {
