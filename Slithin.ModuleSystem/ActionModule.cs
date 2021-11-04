5fbe07d49aa176248f5901a75e850d634ad85de5
filmee24 <filmee24@gmail.com>
2021-11-04 12:50:22 +0100

    Add ModuleSystem Library

---
+++ Slithin.ModuleSystem/ActionModule.cs
@@ -0,0 +1,17 @@
+﻿using WebAssembly;
+
+namespace Slithin.ModuleSystem
+{
+    public class ActionModule
+    {
+        public static Module LoadModule(string path)
+        {
+            var m = Module.ReadFromBinary(path);
+            var wasi = new WASInterface.WASI(m);
+
+            var imports = wasi.CreateImports();
+
+            return m;
+        }
+    }
+}
---
+++ Slithin.ModuleSystem/WASInterface/Sg_wasm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Slithin.ModuleSystem.WASInterface
+{
+    public static class Sg_wasm
+    {
+        public static IntPtr __mem;
+        public static int __mem_size;
+
+        public static Span<byte> GetSpan(int addr, int len)
+        {
+            unsafe
+            {
+                return new Span<byte>((Sg_wasm.__mem + (int)addr).ToPointer(), len);
+            }
+        }
+    }
+}
