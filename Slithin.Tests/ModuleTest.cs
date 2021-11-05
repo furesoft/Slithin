1bf75ca15e2311fc11452fa778b1e4a139201b00
filmee24 <filmee24@gmail.com>
2021-11-05 09:51:44 +0100

    Add ModuleLoader Test

---
+++ Slithin.Tests/ModuleTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Slithin.ModuleSystem;
+
+namespace Slithin.Tests
+{
+    [NUnit.Framework.TestFixture]
+    public static class ModuleTest
+    {
+        [Test]
+        public static void Import()
+        {
+            var m = ActionModule.LoadModule("main.wasm", out var imports);
+
+            var compiled = m.Compile<dynamic>();
+            var instance = compiled(imports);
+
+            var mem = instance.Exports.memory;
+            var id = instance.Exports._start();
+        }
+    }
+}
