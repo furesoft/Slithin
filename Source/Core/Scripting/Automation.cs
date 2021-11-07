dc521c5372c7d3521f0229ea5fd5bbf0af864f86
filmee24 <filmee24@gmail.com>
2021-11-07 09:13:08 +0100

    Cleanup

--- Source/Core/Scripting/Automation.cs
+++ Source/Core/Scripting/Automation.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using NiL.JS;
-using NiL.JS.Core;
-using NiL.JS.Core.Functions;
-using NiL.JS.Extensions;
 using Slithin.Core.Services;
 using Slithin.ModuleSystem;
+using WebAssembly.Runtime;
 
 namespace Slithin.Core.Scripting
 {
     public class Automation
     {
         public List<WebAssembly.Module> Modules = new();
+        private static ImportDictionary _imports = new();
         private readonly IPathManager _pathManager;
 
         public Automation(IPathManager pathManager)
@@ -20,37 +17,18 @@ namespace Slithin.Core.Scripting
             _pathManager = pathManager;
         }
 
+        public ImportDictionary Imports { get => _imports; }
+
         //ToDo: Convert to WASM
         public void Evaluate(string scriptname)
         {
-            var pathManager = ServiceLocator.Container.Resolve<IPathManager>();
-
-            var path = Path.Combine(pathManager.ScriptsDir, scriptname + ".js");
-
-            if (File.Exists(path))
-            {
-                var mainModule = new Module($"Scripts/{scriptname}.js", File.ReadAllText(path));
-                mainModule.Context.DefineVariable("events").Assign(JSValue.Wrap(ServiceLocator.Container.Resolve<EventStorage>()));
-                //mainModule.Context.DefineVariable("config", false).Assign(Utils.ToJSObject((JObject)GetScriptInfo(scriptname).Config));
-                mainModule.Context.DefineVariable("saveConfig").Assign(new ExternalFunction((t, args) =>
-                {
-                    var confObj = Utils.ConvertToJObject(args.First().Value.As<JSObject>());
-
-                    // GetScriptInfo(scriptname).Save(confObj);
-                    return null;
-                }));
-
-                mainModule.ModuleResolversChain.Add(ServiceLocator.Container.Resolve<ModuleResolver>());
-
-                mainModule.Run();
-            }
         }
 
         public void Init()
         {
             foreach (var m in Directory.GetFiles(_pathManager.ScriptsDir, "*.wasm"))
             {
-                var module = ActionModule.LoadModule(m, out var imports);
+                var module = ActionModule.LoadModule(m, out _imports);
 
                 Modules.Add(module);
             }
--- Source/Models/ScriptInfo.cs
+++ Source/Models/ScriptInfo.cs
@@ -1,8 +1,5 @@
-﻿using System.IO;
-using Newtonsoft.Json;
-using Slithin.Core;
+﻿using Slithin.Core;
 using Slithin.Core.Scripting;
-using Slithin.Core.Services;
 
 namespace Slithin.Models
 {
@@ -15,18 +12,6 @@ namespace Slithin.Models
             var automation = ServiceLocator.Container.Resolve<Automation>();
 
             automation.Evaluate(Name);
-        }
-
-        public void Save(object configObj)
-        {
-            Config = configObj;
-
-            var pathManager = ServiceLocator.Container.Resolve<IPathManager>();
-
-            var content = JsonConvert.SerializeObject(this, Formatting.Indented);
-            var file = Path.Combine(pathManager.ConfigBaseDir, "Scripts", Name + ".info");
-
-            File.WriteAllText(file, content);
         }
     }
 }
