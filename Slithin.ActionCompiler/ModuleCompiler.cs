a56c7142afe44ff377b53de9ab1315f7d3c94c20
filmee24 <filmee24@gmail.com>
2021-11-07 09:13:40 +0100

    Add Base To compile Empty Module

--- Slithin.ActionCompiler/ModuleCompiler.cs
+++ Slithin.ActionCompiler/ModuleCompiler.cs
@@ -11,13 +11,20 @@ namespace Slithin.ActionCompiler
         public static Module Compile(string scriptFilename, string infoFilename, string uiFilename)
         {
             var m = new Module();
-            //  m.Imports.Add(new Import.Memory("env", "memory"));
 
             //serialize scriptinfo into data segment
             var info = JsonConvert.DeserializeObject<ScriptInfo>(File.ReadAllText(infoFilename));
             var infoBytes = MessagePackSerializer.Serialize(info);
 
             m.CustomSections.Add(new CustomSection { Name = ".info", Content = new List<byte>(infoBytes) });
+            m.Types.Add(new WebAssemblyType() { });
+
+            m.Exports.Add(new Export("_start"));
+
+            m.Functions.Add(new Function(0));
+            m.Codes.Add(new FunctionBody(new WebAssembly.Instructions.End()));
+
+            m.Imports.Add(new Import.Memory("env", "memory", new Memory(1, 25)));
 
             return m;
         }
