f9a046da264121a9b8a2607b634841e6203c8b9f
Chris <filmee24@gmail.com>
2021-11-19 13:48:16 +0100

    Add WASM Lib

---
+++ Source/Slithin.ModuleSystem.StdLib/StringImplementation.cs
@@ -0,0 +1,14 @@
+﻿namespace Slithin.ModuleSystem.StdLib;
+
+[WasmExport("string")]
+public class StringImplementation
+{
+    [WasmExport("strLen")]
+    public static int strlen(Pointer ptr)
+    {
+        var len = 0;
+        while (ptr[len] != 0) len++;
+
+        return len;
+    }
+}
