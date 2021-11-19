d892268e72d54d0449121327ca0d88d00aa4c577
filmee24 <filmee24@gmail.com>
2021-11-19 13:48:56 +0100

    Add Pointer Overloads

--- Source/Slithin.ModuleSystem/Pointer.cs
+++ Source/Slithin.ModuleSystem/Pointer.cs
@@ -23,14 +23,16 @@ public struct Pointer
         return new Pointer {Base = grain.ToInt32()};
     }
 
+    public int this[int offset] => ReadInt32(offset);
+
     public static implicit operator Pointer(int grain)
     {
         return new Pointer {Base = grain};
     }
 
-    public int ReadInt32()
+    public int ReadInt32(int offset = 0)
     {
-        return Marshal.ReadInt32(Sg_wasm.Mem + Base);
+        return Marshal.ReadInt32(Sg_wasm.Mem + Base + offset);
     }
 
     public void Write(int value)
@@ -41,5 +43,15 @@ public struct Pointer
     public void Write(byte value)
     {
         Marshal.WriteByte(Sg_wasm.Mem + Base, value);
+    }
+
+    public static Pointer operator ++(Pointer ptr)
+    {
+        return new Pointer(ptr.Base + 1);
+    }
+
+    public static Pointer operator --(Pointer ptr)
+    {
+        return new Pointer(ptr.Base - 1);
     }
 }
