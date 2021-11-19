e9ebffd3dd696070a0f37dae587c093c80b513f9
filmee24 <filmee24@gmail.com>
2021-11-19 14:02:34 +0100

    Add Int To String Function

---
+++ Source/Slithin.ModuleSystem.StdLib/ConversionsImplementation.cs
@@ -0,0 +1,11 @@
+﻿namespace Slithin.ModuleSystem.StdLib;
+
+[WasmExport("conversions")]
+public class ConversionsImplementation
+{
+    [WasmExport("intToString")]
+    public static void IntToString(int value, Pointer dst)
+    {
+        dst.Write(value.ToString());
+    }
+}
