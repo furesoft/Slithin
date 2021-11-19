a89f43ef3a5fbf99387148d9ce21a883314daf2d
filmee24 <filmee24@gmail.com>
2021-11-19 14:02:24 +0100

    Add Memory Operation Functions

---
+++ Source/Slithin.ModuleSystem.StdLib/MemoryOperations.cs
@@ -0,0 +1,17 @@
+﻿namespace Slithin.ModuleSystem.StdLib;
+
+[WasmExport("memory")]
+public static class MemoryOperations
+{
+    [WasmExport("memcpy")]
+    public static void Memcpy(Pointer dst, Pointer src, int length)
+    {
+        for (var i = 0; i < length; i++) dst[i] = src[i];
+    }
+
+    [WasmExport("memset")]
+    public static void Memset(Pointer ptr, int ch, int length)
+    {
+        for (var i = 0; i < length; i++) ptr[i] = ch;
+    }
+}
