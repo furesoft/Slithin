87d11f04a0215391f40bdc7206d73bef5fe4a449
filmee24 <filmee24@gmail.com>
2021-11-19 17:15:14 +0100

    Add Empty Allocator

---
+++ Source/Slithin.ModuleSystem.StdLib/Allocator.cs
@@ -0,0 +1,13 @@
+﻿namespace Slithin.ModuleSystem.StdLib;
+
+public class Allocator
+{
+    public static Pointer Allocate(int size)
+    {
+        return default;
+    }
+
+    public static void Free(Pointer ptr)
+    {
+    }
+}
