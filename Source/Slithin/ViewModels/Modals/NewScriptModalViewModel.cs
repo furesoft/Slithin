b0c1dc447133eeeb28173bb75e436cb3f8d8ef58
filmee24 <filmee24@gmail.com>
2021-11-27 15:51:16 +0100

    Cleanup

--- Source/Slithin/ViewModels/Modals/NewScriptModalViewModel.cs
+++ Source/Slithin/ViewModels/Modals/NewScriptModalViewModel.cs
@@ -1,7 +1,31 @@
-﻿using Slithin.Core;
+﻿using System.Collections.ObjectModel;
+using Slithin.Core;
 
 namespace Slithin.ViewModels.Modals;
 
 public class NewScriptModalViewModel : BaseViewModel
 {
+    private string _name;
+
+    private string _selectedCategory;
+
+
+    public NewScriptModalViewModel()
+    {
+        Categories = new ObservableCollection<string>(SyncService.ToolsFilter.Categories);
+    }
+
+    public ObservableCollection<string> Categories { get; set; }
+
+    public string SelectedCategory
+    {
+        get => _selectedCategory;
+        set => SetValue(ref _selectedCategory, value);
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => SetValue(ref _name, value);
+    }
 }
