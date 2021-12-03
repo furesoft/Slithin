ce8c152734be5a4a96d1dcf341d980f27a171f00
filmee24 <filmee24@gmail.com>
2021-12-03 17:14:54 +0100

    Add Core For Parsing

---
+++ Source/Slithin.ActionCompiler/OodParser.cs
@@ -0,0 +1,53 @@
+﻿using Furesoft.Core.CodeDom.CodeDOM;
+using Furesoft.Core.CodeDom.CodeDOM.Expressions.Base;
+using Furesoft.Core.CodeDom.CodeDOM.Expressions.Operators.Binary;
+using Furesoft.Core.CodeDom.CodeDOM.Expressions.Operators.Binary.Arithmetic;
+using Furesoft.Core.CodeDom.CodeDOM.Expressions.Operators.Binary.Assignments;
+using Furesoft.Core.CodeDom.CodeDOM.Expressions.Operators.Binary.Conditional;
+using Furesoft.Core.CodeDom.CodeDOM.Expressions.Operators.Binary.Relational;
+using Furesoft.Core.CodeDom.CodeDOM.Expressions.Operators.Other;
+using Furesoft.Core.CodeDom.CodeDOM.Expressions.Operators.Unary;
+
+namespace Slithin.ActionCompiler
+{
+    public class OodParser
+    {
+        static OodParser()
+        {
+            Negative.AddParsePoints();
+
+            Add.AddParsePoints();
+            Multiply.AddParsePoints();
+            Divide.AddParsePoints();
+            Subtract.AddParsePoints();
+            Assignment.AddParsePoints();
+            Call.AddParsePoints();
+            Expression.AddParsePoints();
+
+            GreaterThan.AddParsePoints();
+            LessThan.AddParsePoints();
+            GreaterThanEqual.AddParsePoints();
+            LessThanEqual.AddParsePoints();
+            NotEqual.AddParsePoints();
+            Equal.AddParsePoints();
+
+            And.AddParsePoints();
+            Or.AddParsePoints();
+            BitwiseXorAssign.AddParsePoints();
+            BitwiseOrAssign.AddParsePoints();
+            BitwiseAndAssign.AddParsePoints();
+
+
+            Mod.AddParsePoints();
+
+            Dot.AddParsePoints();
+        }
+
+        public static CodeUnit ParseFile(string filename)
+        {
+            var tree = CodeUnit.Load(filename);
+
+            return tree;
+        }
+    }
+}
