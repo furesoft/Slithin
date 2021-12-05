6c399a56b7c2d3538db0aeacdac08bf99cfe17af
filmee24 <filmee24@gmail.com>
2021-12-05 10:06:36 +0100

    Add Primitive Class

---
+++ Source/Slithin.ActionCompiler/TypeSystem/Primitive.cs
@@ -0,0 +1,12 @@
+﻿namespace Slithin.ActionCompiler.TypeSystem
+{
+    public class Primitive
+    {
+        public Primitive(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Source/Slithin.ActionCompiler/TypeSystem/Primitives.cs
@@ -0,0 +1,11 @@
+﻿namespace Slithin.ActionCompiler.TypeSystem
+{
+    public static class Primitives
+    {
+        public static Primitive Bool = new Primitive("i8");
+        public static Primitive Int = new Primitive("i32");
+        public static Primitive Long = new Primitive("i64");
+        public static Primitive Float = new Primitive("f32");
+        public static Primitive Double = new Primitive("f64");
+    }
+}
