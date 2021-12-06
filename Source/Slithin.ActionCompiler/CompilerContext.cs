49839437d8eb96f1ba1244721a84f095fa7da1a4
filmee24 <filmee24@gmail.com>
2021-12-06 18:59:55 +0100

    Add References To CompilerContext

--- Source/Slithin.ActionCompiler/CompilerContext.cs
+++ Source/Slithin.ActionCompiler/CompilerContext.cs
@@ -7,25 +7,27 @@ namespace Slithin.ActionCompiler;
 
 public class CompilerContext
 {
-    public List<CodeUnit> Trees { get; set; } = new();
+    [Option('s', "symbol", HelpText = "Symbolicon filename")]
+    public string ImageFilename { get; set; }
 
     [Option('i', "input", Required = true, HelpText = "Input files to be compiled.")]
     public IEnumerable<string> InputFiles { get; set; }
 
-    [Option('o', "output", Required = true, HelpText = "Output filename")]
-    public string OutputFilename { get; set; }
-
     [Option('l', "library", HelpText = "The compiled module is a library")]
     public bool IsLibray { get; set; }
-
-    [Option('u', "ui", Required = true, HelpText = "UI filename")]
-    public string UiFilename { get; set; }
 
     [Option('m', "metadata", Required = true, HelpText = "Metadata filename")]
     public string MetadataFilename { get; set; }
 
-    [Option('s', "symbol", HelpText = "Symbolicon filename")]
-    public string ImageFilename { get; set; }
+    [Option('o', "output", Required = true, HelpText = "Output filename")]
+    public string OutputFilename { get; set; }
+
+    [Option('r', "reference", Required = false, HelpText = "Reference that should be linked")]
+    public IEnumerable<string> References { get; set; }
 
     public Module ResultModule { get; set; } = new();
+    public List<CodeUnit> Trees { get; set; } = new();
+
+    [Option('u', "ui", Required = true, HelpText = "UI filename")]
+    public string UiFilename { get; set; }
 }
