243692c6e9f9fbf187b45b26171b5c9dbcc83036
filmee24 <filmee24@gmail.com>
2021-12-10 17:21:41 +0100

    Move VPL To Window

--- Source/Slithin/ViewModels/Modals/NewScriptModalViewModel.cs
+++ Source/Slithin/ViewModels/Modals/NewScriptModalViewModel.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
-using NodeEditor.Controls;
 using NodeEditor.Model;
 using NodeEditor.Serializer;
 using Slithin.Core;
 using Slithin.UI;
+using Slithin.VPL;
 
 namespace Slithin.ViewModels.Modals;
 
@@ -15,13 +15,12 @@ public class NewScriptModalViewModel : BaseViewModel
 
     private readonly INodeSerializer _serializer;
     private IDrawingNode? _drawing;
+    private bool _isEditMode = true;
     private string _name;
 
     private string _selectedCategory;
     private int _step;
     private IList<INodeTemplate>? _templates;
-
-    private bool _isEditMode = true;
 
     public NewScriptModalViewModel()
     {
@@ -36,16 +35,22 @@ public class NewScriptModalViewModel : BaseViewModel
         Drawing.Serializer = _serializer;
     }
 
-    public IList<INodeTemplate>? Templates
-    {
-        get => _templates;
-        set => SetValue(ref _templates, value);
-    }
-
     public IDrawingNode? Drawing
     {
         get => _drawing;
         set => SetValue(ref _drawing, value);
+    }
+
+    public bool IsEditMode
+    {
+        get => _isEditMode;
+        set => SetValue(ref _isEditMode, value);
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => SetValue(ref _name, value);
     }
 
     public ICommand NextCommand { get; set; }
@@ -56,30 +61,26 @@ public class NewScriptModalViewModel : BaseViewModel
         set => SetValue(ref _selectedCategory, value);
     }
 
-    public bool IsEditMode
-    {
-        get => _isEditMode;
-        set => SetValue(ref _isEditMode, value);
-    }
-
     public int Step
     {
         get => _step;
         set => SetValue(ref _step, value);
     }
 
-    public string Name
+    public IList<INodeTemplate>? Templates
     {
-        get => _name;
-        set => SetValue(ref _name, value);
+        get => _templates;
+        set => SetValue(ref _templates, value);
     }
-
 
     private void Next(object obj)
     {
         if (Step == 0)
         {
             Step++;
+
+            var window = new VplWindow();
+            window.Show();
         }
 
         _factory.PrintNetList(Drawing);
