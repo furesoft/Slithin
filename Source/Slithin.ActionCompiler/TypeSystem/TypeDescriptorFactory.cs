81ffff39f94e1c284d472cadbeb415255f03836d
filmee24 <filmee24@gmail.com>
2021-12-10 11:11:47 +0100

    Add TypeDescriptorFactory

---
+++ Source/Slithin.ActionCompiler/TypeSystem/TypeDescriptorFactory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Slithin.ActionCompiler.TypeSystem
+{
+    public static class TypeDescriptorFactory
+    {
+        private static readonly Dictionary<string, TypeDescriptor> _primitives = new();
+
+        static TypeDescriptorFactory()
+        {
+            _primitives.Add("i32", Primitives.Int);
+            _primitives.Add("i64", Primitives.Long);
+            _primitives.Add("f32", Primitives.Float);
+            _primitives.Add("f64", Primitives.Double);
+        }
+
+        public static TypeDescriptor FromTypeName(string typename)
+        {
+            if (_primitives.ContainsKey(typename))
+            {
+                return _primitives[typename];
+            }
+            else
+            {
+                return null; //Todo: implement custom types
+            }
+        }
+    }
+}
