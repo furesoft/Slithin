6a0a10fe38cd89fe844dfd41724b937ee0caa177
filmee24 <filmee24@gmail.com>
2021-12-10 11:05:29 +0100

    Add TypeDescriptor

--- Source/Slithin.ActionCompiler/TypeSystem/Primitive.cs
+++ Source/Slithin.ActionCompiler/TypeSystem/Primitive.cs
@@ -1,48 +1,10 @@
-﻿using System.Collections.Generic;
-
-namespace Slithin.ActionCompiler.TypeSystem
+﻿namespace Slithin.ActionCompiler.TypeSystem
 {
-    public class Primitive
+    public class Primitive : TypeDescriptor
     {
-        public Dictionary<string, List<OperatorInfo>> Operators = new();
-
         public Primitive(string name, uint token)
+            : base(name, token)
         {
-            Name = name;
-            Token = token;
-        }
-
-        public string Name { get; set; }
-        public uint Token { get; set; }
-
-        public OperatorInfo this[string op]
-        {
-            set
-            {
-                Add(op, value);
-            }
-        }
-
-        public void Add(string op, OperatorInfo operatorInfo)
-        {
-            if (Operators.ContainsKey(op))
-            {
-                Operators[op].Add(operatorInfo);
-            }
-            else
-            {
-                var list = new List<OperatorInfo>
-                {
-                    operatorInfo
-                };
-
-                Operators.Add(op, list);
-            }
-        }
-
-        public override string ToString()
-        {
-            return Name;
         }
     }
 }
