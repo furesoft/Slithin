158848db0a898dbf455e1460293e55c2fa60d574
filmee24 <filmee24@gmail.com>
2021-12-11 17:37:13 +0100

    Add ArrayTypeDescriptor

---
+++ Source/Slithin.ActionCompiler/TypeSystem/ArrayTypeDescriptor.cs
@@ -0,0 +1,19 @@
+﻿namespace Slithin.ActionCompiler.TypeSystem
+{
+    public class ArrayTypeDescriptor : TypeDescriptor
+    {
+        public ArrayTypeDescriptor(TypeDescriptor type, int[] dimensions) : base($"Array<{type.Name}>", (int)PrimitiveTypeTokens.Array)
+        {
+            ArrayType = type;
+            Dimensions = dimensions;
+        }
+
+        public TypeDescriptor ArrayType { get; set; }
+        public int[] Dimensions { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ArrayType.Name}[{string.Join(",", Dimensions)}]";
+        }
+    }
+}
