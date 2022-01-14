4e8dedf58f86c0bcf35dcc0076d4511e8e76c8ca
filmee24 <filmee24@gmail.com>
2022-01-14 16:45:09 +0100

    Add FeatureEnabler

---
+++ Source/Slithin/FeatureEnabler.cs
@@ -0,0 +1,21 @@
+﻿using FeatureSwitcher.Configuration;
+using Slithin.Core.Features;
+
+namespace Slithin;
+
+public class FeatureEnabler
+{
+    public void Init()
+    {
+#if DEBUG
+        Features.Are.ConfiguredBy.Custom(Features.OfType<SharableFeature>.Enabled);
+        Features.Are.ConfiguredBy.Custom(Features.OfType<ExportFeature>.Enabled);
+        Features.Are.ConfiguredBy.Custom(Features.OfType<ExportPdfFeature>.Enabled);
+        Features.Are.ConfiguredBy.Custom(Features.OfType<ExportSvgFeature>.Enabled);
+        Features.Are.ConfiguredBy.Custom(Features.OfType<ExportPngFeature>.Enabled);
+        Features.Are.ConfiguredBy.Custom(Features.OfType<MigrationFeature>.Enabled);
+#else
+
+#endif
+    }
+}
