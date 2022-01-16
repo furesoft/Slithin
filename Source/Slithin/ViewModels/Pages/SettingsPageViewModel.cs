b763c0b52eb1f9c7079d335d14b2d0a7cb1fc36c
filmee24 <filmee24@gmail.com>
2022-01-16 17:05:42 +0100

    Add External Buttons To Settings

--- Source/Slithin/ViewModels/Pages/SettingsPageViewModel.cs
+++ Source/Slithin/ViewModels/Pages/SettingsPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Slithin.Core;
+﻿using System.Windows.Input;
+using Slithin.Core;
+using Slithin.Core.Scripting;
 using Slithin.Core.Services;
 
 namespace Slithin.ViewModels.Pages;
@@ -12,6 +14,8 @@ public class SettingsPageViewModel : BaseViewModel
     public SettingsPageViewModel(ILoginService loginService)
     {
         DeviceName = loginService.GetCurrentCredential().Name;
+
+        OpenExternalCommand = new DelegateCommand(OpenExternal);
     }
 
     public bool AutomaticUpdates
@@ -24,5 +28,12 @@ public class SettingsPageViewModel : BaseViewModel
     {
         get { return _deviceName; }
         set { _deviceName = value; }
+    }
+
+    public ICommand OpenExternalCommand { get; set; }
+
+    private void OpenExternal(object obj)
+    {
+        Utils.OpenUrl(obj.ToString());
     }
 }
