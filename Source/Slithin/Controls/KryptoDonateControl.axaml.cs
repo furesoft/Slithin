890b951cd3fc7a886f8a2024a6c9bf0e1d03ab14
filmee24 <filmee24@gmail.com>
2022-01-16 14:38:15 +0100

    Add KryptodonateControl

---
+++ Source/Slithin/Controls/KryptoDonateControl.axaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+using Slithin.Core;
+
+namespace Slithin.Controls;
+
+public class KryptoDonateControl : TemplatedControl
+{
+    public static StyledProperty<string> AddressProperty = AvaloniaProperty.Register<KryptoDonateControl, string>(nameof(Address), "0000000000000000000000");
+    public static StyledProperty<ICommand> CopyAddressCommandProperty = AvaloniaProperty.Register<KryptoDonateControl, ICommand>(nameof(CopyAddressCommand));
+    public static StyledProperty<IImage> ImageProperty = AvaloniaProperty.Register<KryptoDonateControl, IImage>(nameof(Image));
+
+    public KryptoDonateControl()
+    {
+        CopyAddressCommand = new DelegateCommand(CopyAddress);
+    }
+
+    public string Address
+    {
+        get { return GetValue(AddressProperty); }
+        set { SetValue(AddressProperty, value); }
+    }
+
+    public ICommand CopyAddressCommand
+    {
+        get { return GetValue(CopyAddressCommandProperty); }
+        set { SetValue(CopyAddressCommandProperty, value); }
+    }
+
+    public IImage Image
+    {
+        get { return GetValue(ImageProperty); }
+        set { SetValue(ImageProperty, value); }
+    }
+
+    private async void CopyAddress(object obj)
+    {
+        await Application.Current.Clipboard.SetTextAsync(Address);
+    }
+}
