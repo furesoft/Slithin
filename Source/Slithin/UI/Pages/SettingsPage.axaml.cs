87c3b92ea2e33420826a11b39c4501946ddb0c49
filmee24 <filmee24@gmail.com>
2022-01-16 16:49:35 +0100

    Add Settings

--- Source/Slithin/UI/Pages/SettingsPage.axaml.cs
+++ Source/Slithin/UI/Pages/SettingsPage.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Markup.Xaml;
 using Slithin.Core;
 using Slithin.Core.Features;
 using Slithin.Core.FeatureToggle;
+using Slithin.ViewModels.Pages;
 
 namespace Slithin.UI.Pages;
 
@@ -27,5 +28,7 @@ public partial class SettingsPage : UserControl, IPage
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+
+        DataContext = ServiceLocator.Container.Resolve<SettingsPageViewModel>();
     }
 }
---
+++ Source/Slithin/ViewModels/Pages/SettingsPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Slithin.Core;
+using Slithin.Core.Services;
+
+namespace Slithin.ViewModels.Pages;
+
+public class SettingsPageViewModel : BaseViewModel
+{
+    private bool _automaticUpdates;
+
+    private string _deviceName;
+
+    public SettingsPageViewModel(ILoginService loginService)
+    {
+        DeviceName = loginService.GetCurrentCredential().Name;
+    }
+
+    public bool AutomaticUpdates
+    {
+        get { return _automaticUpdates; }
+        set { SetValue(ref _automaticUpdates, value); }
+    }
+
+    public string DeviceName
+    {
+        get { return _deviceName; }
+        set { _deviceName = value; }
+    }
+}
