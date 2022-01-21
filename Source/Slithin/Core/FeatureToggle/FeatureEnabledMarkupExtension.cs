751c976f86675920ad99b51301aaa4df11ea1e68
filmee24 <filmee24@gmail.com>
2022-01-21 16:24:02 +0100

    Add FeatureEnabledMarkupExtension

---
+++ Source/Slithin/Core/FeatureToggle/FeatureEnabledMarkupExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Avalonia.Markup.Xaml;
+
+namespace Slithin.Core.FeatureToggle;
+
+public class FeatureEnabledMarkupExtension : MarkupExtension
+{
+    public FeatureEnabledMarkupExtension(string featureName)
+    {
+        FeatureName = featureName;
+    }
+
+    public string FeatureName { get; set; }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return Features.FromString(FeatureName).IsEnabled;
+    }
+}
