09f0d1d10cf7e5d366bbe3dc08599db926cea7b6
filmee24 <filmee24@gmail.com>
2022-01-22 14:11:38 +0100

    Add Visual Nodes

---
+++ Source/Slithin/VPL/Components/ViewModels/GetCurrentDateTimeNode.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.Serialization;
+using Slithin.VPL.Components.Views;
+using Slithin.VPL.NodeBuilding;
+
+namespace Slithin.VPL.Components.ViewModels;
+
+[DataContract(IsReference = true)]
+[NodeView(typeof(GetCurrentDateTimeView))]
+[IgnoreTemplate]
+public class GetCurrentDateTimeNode : VisualNode
+{
+    public GetCurrentDateTimeNode() : base("CurrentDateTime")
+    {
+    }
+
+    [Pin("Date")]
+    public IInputPin DatePin { get; set; }
+
+    [Pin("Flow Input")]
+    public IInputPin FlowInputPin { get; set; }
+
+    [Pin("Flow Output")]
+    public IOutputPin FlowPin { get; set; }
+
+    [Pin("Time")]
+    public IInputPin TimePin { get; set; }
+}
+
+[DataContract(IsReference = true)]
+[NodeView(typeof(GetCurrentDateTimeView))]
+[IgnoreTemplate]
+public class LoopNode : VisualNode
+{
+    public LoopNode() : base("Repeat")
+    {
+    }
+
+    [Pin("Condition")]
+    public IInputPin ConditionPin { get; set; }
+
+    [Pin("Flow Input")]
+    public IInputPin FlowInputPin { get; set; }
+
+    [Pin("Flow Output")]
+    public IOutputPin FlowPin { get; set; }
+}
