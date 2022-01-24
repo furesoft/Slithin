7a06dc11a5d130518a839c85993037d5a42e189a
filmee24 <filmee24@gmail.com>
2022-01-24 15:08:27 +0100

    Add Interpreter Visitor

---
+++ Source/Slithin.Scripting/Execution/Interpreter.cs
@@ -0,0 +1,38 @@
+﻿using Slithin.Scripting.Parsing.AST;
+using Slithin.Scripting.Parsing.AST.Expressions;
+
+namespace Slithin.Scripting.Execution
+{
+    internal class Interpreter : IVisitor<object>
+    {
+        public object Visit(InvalidNode invalidNode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Visit(LiteralNode literal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Visit(CompilationUnit compilationUnit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Visit(Block block)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Visit(InvalidExpr invalidExpr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Visit(NameExpression nameExpression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
