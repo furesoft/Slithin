3afac5d7beb1a3f649c88cb0143ee6a982e823ec
filmee24 <filmee24@gmail.com>
2022-02-23 20:13:22 +0100

    Add Specifiying Port

--- Source/Slithin/Core/Validators/LoginInfoValidator.cs
+++ Source/Slithin/Core/Validators/LoginInfoValidator.cs
@@ -8,7 +8,7 @@ public class LoginInfoValidator : AbstractValidator<LoginInfo>
     public LoginInfoValidator()
     {
         RuleFor(x => x.IP).NotEmpty()
-            .Matches(@"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\:[0-9]+)?$");
+            .Matches(IPAddress.Pattern);
         RuleFor(x => x.Password).NotEmpty().MinimumLength(4);
     }
 }
