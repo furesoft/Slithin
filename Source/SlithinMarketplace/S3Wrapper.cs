e857279b85a5386950ccbe74d3cc4f53800b77b6
filmee24 <filmee24@gmail.com>
2022-02-26 14:48:47 +0100

    Add Core for Marketplace API

---
+++ Source/SlithinMarketplace/S3Wrapper.cs
@@ -0,0 +1,112 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace SlithinMarketplace;
+
+public class S3Wrapper
+{
+    private AmazonS3Client client;
+
+    public S3Wrapper(AmazonS3Client client)
+    {
+        this.client = client;
+    }
+
+    public FileInfo GetObject(string bucket, string key, string output)
+    {
+        string name = Path.GetFileName(key);
+        string outputPath = Path.Join(output, name);
+        Console.WriteLine("Downloading: " + key + " to " + outputPath);
+
+        try
+        {
+            GetObjectRequest request = new()
+            {
+                BucketName = bucket,
+                Key = key
+            };
+
+            var task = client.GetObjectAsync(request);
+            task.Wait();
+            GetObjectResponse response = task.Result;
+            Stream responseStream = response.ResponseStream;
+            FileStream fileStream = File.OpenWrite(outputPath);
+            responseStream.CopyTo(fileStream);
+            fileStream.Close();
+        }
+        catch (AmazonS3Exception e)
+        {
+            // If the bucket or the object do not exist
+            Console.WriteLine($"Error: '{e.Message}'");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Unhandled Error: {e.Message}");
+        }
+
+        return new FileInfo(outputPath);
+    }
+
+    public List<S3Object> ListObjects(string bucketName)
+    {
+        Console.WriteLine("Listing files from Bucket: " + bucketName);
+        List<S3Object> result = new();
+        try
+        {
+            ListObjectsV2Request request = new()
+            {
+                BucketName = bucketName,
+                MaxKeys = 5
+            };
+
+            var response = new ListObjectsV2Response();
+            do
+            {
+                var task = client.ListObjectsV2Async(request);
+                task.Wait();
+                response = task.Result;
+                result.AddRange(response.S3Objects);
+
+                // If the response is truncated, set the request ContinuationToken
+                // from the NextContinuationToken property of the response.
+                request.ContinuationToken = response.NextContinuationToken;
+            } while (response.IsTruncated);
+        }
+        catch (AmazonS3Exception e)
+        {
+            Console.WriteLine($"Error:'{e.Message}'");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Unhandled Error: {e.Message}");
+        }
+
+        return result;
+    }
+
+    public void UploadObjectFromFile(string bucketName, string objectName, string filePath)
+    {
+        Console.WriteLine("Uploading: " + filePath + " to " + bucketName + " >> " + objectName);
+        try
+        {
+            PutObjectRequest putRequest = new()
+            {
+                BucketName = bucketName,
+                Key = objectName,
+                InputStream = File.OpenRead(filePath)
+            };
+
+            var task = this.client.PutObjectAsync(putRequest);
+            task.Wait();
+            PutObjectResponse response = task.Result;
+        }
+        catch (AmazonS3Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Unhandled Error: {e.Message}");
+        }
+    }
+}
