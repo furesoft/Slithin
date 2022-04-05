14564a5ec3a4981e6bd9e11a01a9757b5776b77a
filmee24 <filmee24@gmail.com>
2022-04-05 21:50:05 +0200

    Add SettingsStep

---
+++ Source/Slithin/UI/FirstStartSteps/SettingsStep.axaml.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Slithin.Core;
+using Slithin.ViewModels.Pages;
+
+namespace Slithin.UI.FirstStartSteps;
+
+public partial class SettingsStep : UserControl
+{
+    public SettingsStep()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+
+        var vm = ServiceLocator.Container.Resolve<SettingsPageViewModel>();
+        vm.Load();
+
+        DataContext = vm;
+    }
+}
