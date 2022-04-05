662067f6ffd350d7a077c421a3491f9946a57c2f
filmee24 <filmee24@gmail.com>
2022-04-05 18:16:02 +0200

    Add DeviceStep

---
+++ Source/Slithin/UI/FirstStartSteps/DeviceStep.axaml.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Slithin.Core;
+using Slithin.ViewModels;
+
+namespace Slithin.UI.FirstStartSteps;
+
+public partial class DeviceStep : UserControl
+{
+    public DeviceStep()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+
+        var vm = ServiceLocator.Container.Resolve<AddDeviceWindowViewModel>();
+        vm.Load();
+
+        DataContext = vm;
+    }
+}
