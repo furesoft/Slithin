257d0694b089a6a6b19f7f39aad208f885211d69
filmee24 <filmee24@gmail.com>
2022-04-24 20:32:48 +0200

    Add Models

--- Source/Slithin.Marketplace.Models/AssetModel.cs
+++ Source/Slithin.Marketplace.Models/AssetModel.cs
@@ -6,10 +6,13 @@ public class AssetModel
     public string FileID { get; set; }
     public string ID { get; set; }
     public DateTime UploadTime { get; set; }
+<<<<<<< develop
 
     public void InitIDs()
     {
         ID = Guid.NewGuid().ToString();
         FileID = Guid.NewGuid().ToString();
     }
+=======
+>>>>>>> Add Models
 }
