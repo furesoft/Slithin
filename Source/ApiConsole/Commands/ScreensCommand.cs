2590d4328b18c99e9dc87fe422e671f9c2ac923a
filmee24 <filmee24@gmail.com>
2022-04-24 21:06:22 +0200

    Add Commands

---
+++ Source/ApiConsole/Commands/ScreensCommand.cs
@@ -0,0 +1,13 @@
+﻿using ApiConsole.Core;
+using CommandLine;
+
+namespace ApiConsole.Commands;
+
+[Verb("screens", HelpText = "Act with screens")]
+public class ScreensCommand : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Executing screens");
+    }
+}
---
+++ Source/ApiConsole/Commands/TemplatesCommand.cs
@@ -0,0 +1,13 @@
+﻿using ApiConsole.Core;
+using CommandLine;
+
+namespace ApiConsole.Commands;
+
+[Verb("templates", HelpText = "Act with templates")]
+public class TemplatesCommand : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Executing templates");
+    }
+}
