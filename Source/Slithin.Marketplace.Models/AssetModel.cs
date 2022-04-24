fde8001d9f1fa12cbba96e6f3d4357b75a58dbdf
filmee24 <filmee24@gmail.com>
2022-04-24 20:32:48 +0200

    Add Models

--- Source/Slithin.Marketplace.Models/AssetModel.cs
+++ Source/Slithin.Marketplace.Models/AssetModel.cs
@@ -5,4 +5,5 @@ public class AssetModel
     public string CreatorID { get; set; }
     public string FileID { get; set; }
     public string ID { get; set; }
+    public DateTime UploadTime { get; set; }
 }
