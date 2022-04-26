ef5498343a122282a720a1c87705de1f15d6e4ec
Ralph Schuler <ralph@inhaus.ai>
2022-04-26 21:34:47 +0200

    fix "us-east1"

--- Source/SlithinMarketplace/ServiceLocator.cs
+++ Source/SlithinMarketplace/ServiceLocator.cs
@@ -9,7 +9,7 @@ public class ServiceLocator
         AmazonS3Config config = new AmazonS3Config();
         config.ServiceURL = Environment.GetEnvironmentVariable("MINIO_HOST");
         config.ForcePathStyle = true;
-        config.AuthenticationRegion = "us-east1";
+        config.AuthenticationRegion = AuthenticationRegion.USEast1.SystemName;
 
         AmazonS3Client client = new(
             Environment.GetEnvironmentVariable("MINIO_ACCESS_KEY"),
