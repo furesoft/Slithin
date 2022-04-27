d491a91f774f88898715f33f15dd0a6d40f72947
Chris <furesoft@users.noreply.github.com>
2022-04-27 21:11:05 +0200

    Cleanup (#151)
    * Cleanup
    
    * Change API URL To Online Version
    
    * Fix Colors On Cards
    
    * Add Uploading Templates

--- Source/Slithin.Marketplace.Models/Template.cs
+++ Source/Slithin.Marketplace.Models/Template.cs
@@ -5,6 +5,9 @@ namespace Slithin.Marketplace.Models;
 public sealed class Template : AssetModel
 {
     public string[] Categories { get; set; }
+    public string Filename { get; set; }
+    public string IconCode { get; set; }
     public bool IsLandscape { get; set; }
     public string Name { get; set; }
+    public string SvgFileID { get; set; }
 }
