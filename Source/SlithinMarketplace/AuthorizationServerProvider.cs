3f68e511ebca7c7601d1f99f12f2be293680d911
filmee24 <filmee24@gmail.com>
2022-05-14 20:22:45 +0200

    Cleanup

--- Source/SlithinMarketplace/AuthorizationServerProvider.cs
+++ Source/SlithinMarketplace/AuthorizationServerProvider.cs
@@ -19,7 +19,7 @@ internal class AuthorizationServerProvider : IAuthorizationServerProvider
             if (user == null || Utils.ComputeSha256Hash(data.Password) != user.HashedPassword)
             {
                 context.Rejected();
-                context.Validated(string.Empty);
+                //context.Validated(string.Empty);
             }
 
             context.Validated(data.Username);
