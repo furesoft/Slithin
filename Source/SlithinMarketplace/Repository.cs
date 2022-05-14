b27243b9f6feab12b723a5460d6918c9b9b715ee
Chris <furesoft@users.noreply.github.com>
2022-05-14 23:02:34 +0200

    Cleanup (#194)
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup

--- Source/SlithinMarketplace/Repository.cs
+++ Source/SlithinMarketplace/Repository.cs
@@ -64,8 +64,7 @@ public class Repository
 
     public User GetUser(string username)
     {
-        Console.WriteLine($"{username}");
-
+        Console.WriteLine($"'{username}'");
         var user = ServiceLocator.Database.GetCollection<User>("users")
             .Find(Builders<User>.Filter.Eq(_ => _.Username, username)).Single();
 
