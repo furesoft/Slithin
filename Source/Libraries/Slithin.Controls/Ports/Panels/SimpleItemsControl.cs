15d3daede97c2fb1bd34c080a44586b55c426139
filmee24 <filmee24@gmail.com>
2022-05-15 17:56:57 +0200

    Add RatingControl

---
+++ Source/Libraries/Slithin.Controls/Ports/Panels/SimpleItemsControl.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using Avalonia;
+using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+
+namespace Slithin.Controls.Ports.Panels;
+
+//ported from https://github.com/HandyOrg/HandyControl.git
+
+/// <summary>
+/// 'itemscontrol' which use a panel to add it's children
+/// </summary>
+public class SimpleItemsControl : TemplatedControl
+{
+    /// <summary>
+    /// Defines the HasItems property.
+    /// </summary>
+    public static readonly StyledProperty<bool> HasItemsProperty =
+    AvaloniaProperty.Register<SimpleItemsControl, bool>(nameof(HasItems));
+
+    /// <summary>
+    /// Defines the ItemsSource property.
+    /// </summary>
+    public static readonly StyledProperty<IEnumerable> ItemsSourceProperty =
+    AvaloniaProperty.Register<SimpleItemsControl, IEnumerable>(nameof(ItemsSource));
+
+    /// <summary>
+    /// Defines the ItemTemplate property.
+    /// </summary>
+    public static readonly StyledProperty<IDataTemplate> ItemTemplateProperty =
+    AvaloniaProperty.Register<SimpleItemsControl, IDataTemplate>(nameof(ItemTemplate));
+
+    private const string ElementPanel = "PART_Panel";
+
+    public SimpleItemsControl()
+    {
+        ItemTemplateProperty.Changed.AddClassHandler<SimpleItemsControl>((o, e) => OnItemTemplateChanged(o, e));
+        ItemsSourceProperty.Changed.AddClassHandler<SimpleItemsControl>((o, e) => OnItemsSourceChanged(o, e));
+
+        var items = new AvaloniaList<object>();
+        items.CollectionChanged += (s, e) =>
+        {
+            if (e.NewItems != null && e.NewItems.Count > 0)
+            {
+                SetValue(HasItemsProperty, true);
+            }
+            OnItemsChanged(s, e);
+        };
+        Items = items;
+    }
+
+    /// <summary>
+    /// Gets or sets HasItems.
+    /// </summary>
+    public bool HasItems
+    {
+        get { return GetValue(HasItemsProperty); }
+        private set { SetValue(HasItemsProperty, value); }
+    }
+
+    public IEnumerable Items { get; internal set; }
+
+    /// <summary>
+    /// Gets or sets ItemsSource.
+    /// </summary>
+    public IEnumerable ItemsSource
+    {
+        get { return GetValue(ItemsSourceProperty); }
+        set { SetValue(ItemsSourceProperty, value); }
+    }
+
+    /// <summary>
+    /// Gets or sets ItemTemplate.
+    /// </summary>
+    [Bindable(true)]
+    public IDataTemplate ItemTemplate
+    {
+        get { return GetValue(ItemTemplateProperty); }
+        set { SetValue(ItemTemplateProperty, value); }
+    }
+
+    internal IPanel ItemsHost { get; set; }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        ItemsHost?.Children.Clear();
+        base.OnApplyTemplate(e);
+
+        ItemsHost = e.NameScope.Find<IPanel>(ElementPanel);
+        Refresh();
+    }
+
+    protected virtual void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue)
+    {
+    }
+
+    protected virtual void OnItemTemplateChanged(SimpleItemsControl o, AvaloniaPropertyChangedEventArgs e)
+    {
+        Refresh();
+    }
+
+    protected virtual void Refresh()
+    {
+        if (ItemsHost == null)
+            return;
+
+        ItemsHost.Children.Clear();
+        foreach (var item in Items)
+        {
+            if (item is TemplatedControl element)
+            {
+                //element.Style = ItemContainerStyle;
+                ItemsHost.Children.Add(element);
+            }
+        }
+    }
+
+    protected virtual void UpdateItems()
+    {
+    }
+
+    private void OnItemsChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        Refresh();
+        UpdateItems();
+    }
+
+    private void OnItemsSourceChanged(SimpleItemsControl o, AvaloniaPropertyChangedEventArgs e)
+    {
+        o.OnItemsSourceChanged((IEnumerable)e.OldValue, (IEnumerable)e.NewValue);
+    }
+}
---
+++ Source/Libraries/Slithin.Controls/Ports/RoutedPropertyChangedEventArgs.cs
@@ -0,0 +1,57 @@
+﻿using Avalonia.Interactivity;
+
+namespace Slithin.Controls.Ports;
+
+// This source file is adapted from the Windows Presentation Foundation project.
+// (https://github.com/dotnet/wpf/)
+
+/// <summary>
+///     This delegate must used by handlers of the RoutedPropertyChangedEvent event.
+/// </summary>
+/// <param name="sender">The current element along the event's route.</param>
+/// <param name="e">The event arguments containing additional information about the event.</param>
+/// <returns>Nothing.</returns>
+public delegate void RoutedPropertyChangedEventHandler<T>(object sender, RoutedPropertyChangedEventArgs<T> e);
+
+/// <summary>
+/// This RoutedPropertyChangedEventArgs class contains old and new value when
+/// RoutedPropertyChangedEvent is raised.
+/// </summary>
+/// <seealso cref="RoutedEventArgs" />
+/// <typeparam name="T"></typeparam>
+public class RoutedPropertyChangedEventArgs<T> : RoutedEventArgs
+{
+    /// <summary>
+    /// constructs RoutedPropertyChangedEventArgs by parameter and routed event
+    /// </summary>
+    /// <param name="oldValue"></param>
+    /// <param name="newValue"></param>
+    /// <param name="routedEvent"></param>
+    public RoutedPropertyChangedEventArgs(T oldValue, T newValue, RoutedEvent routedEvent)
+    {
+        OldValue = oldValue;
+        NewValue = newValue;
+        RoutedEvent = routedEvent;
+    }
+
+    /// <summary>
+    /// constructs RoutedPropertyChangedEventArgs by parameter
+    /// </summary>
+    /// <param name="oldValue"></param>
+    /// <param name="newValue"></param>
+    public RoutedPropertyChangedEventArgs(T oldValue, T newValue)
+    {
+        OldValue = oldValue;
+        NewValue = newValue;
+    }
+
+    /// <summary>
+    /// new value
+    /// </summary>
+    public T NewValue { get; }
+
+    /// <summary>
+    /// old value
+    /// </summary>
+    public T OldValue { get; }
+}
