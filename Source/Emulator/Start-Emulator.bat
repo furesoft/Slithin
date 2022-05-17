91518009b3045abe73eb4f3b9a3c296b0b7f18ad
filmee24 <filmee24@gmail.com>
2022-05-17 15:25:57 +0200

    Clenaup

--- Source/Emulator/Start-Emulator.bat
+++ Source/Emulator/Start-Emulator.bat
@@ -1,4 +1,4 @@
 docker build -t remarkable-emulator .
-docker run -it -p 22:22 --rm -v C:\Users\chris\Desktop\re\:/re remarkable-emulator
+docker run -it -p 22:22 --rm -v %CD%\\remarkable_data:/re remarkable-emulator
 
 PAUSE
