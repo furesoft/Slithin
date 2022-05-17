5b3db2fed894fc94f6d380ab398090a760f48a26
filmee24 <filmee24@gmail.com>
2022-05-17 16:02:57 +0200

    Add SampleData

--- Source/Emulator/Start-Emulator.bat
+++ Source/Emulator/Start-Emulator.bat
@@ -1,4 +1,4 @@
 docker build -t remarkable-emulator .
-docker run -it -p 22:22 --rm -v %CD%\\remarkable_data:/re remarkable-emulator
+docker run -it -p 22:22 --rm -v %CD%\\remarkable_data:/remarkable_data remarkable-emulator
 
 PAUSE
