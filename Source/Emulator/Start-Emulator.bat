12a43fc2b6543dbaee9c180d3ad5e5500fc6fbc2
filmee24 <filmee24@gmail.com>
2022-05-17 13:50:50 +0200

    Try To Fix Emulator

--- Source/Emulator/Start-Emulator.bat
+++ Source/Emulator/Start-Emulator.bat
@@ -1,1 +1,4 @@
-docker build -t remarkable-emulator . && docker run -it -p 22:22 --rm -v C:\Users\chris\Desktop\re\:/re remarkable-emulator
+docker build -t remarkable-emulator .
+docker run -it -p 22:22 --rm -v C:\Users\chris\Desktop\re\:/re remarkable-emulator
+
+PAUSE
