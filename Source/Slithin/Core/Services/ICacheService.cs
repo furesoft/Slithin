bdb0e6ca9103b2b266100ce7cc46f8ff047b9d57
filmee24 <filmee24@gmail.com>
2022-06-26 10:07:06 +0200

    Improve Cache

--- Source/Slithin/Core/Services/ICacheService.cs
+++ Source/Slithin/Core/Services/ICacheService.cs
@@ -1,8 +1,10 @@
-﻿namespace Slithin.Core.Services;
+﻿using System;
+
+namespace Slithin.Core.Services;
 
 public interface ICacheService
 {
     void AddObject<T>(string name, T obj);
 
-    public T GetObject<T>(string name, T obj = default);
+    public T GetObject<T>(string name, Func<T> obj = default);
 }
