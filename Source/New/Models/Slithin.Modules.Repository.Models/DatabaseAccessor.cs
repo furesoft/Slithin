5015ef6a7852674c3d860206cd1888276f96f0eb
filmee24 <filmee24@gmail.com>
2022-12-18 13:29:28 +0100

    Cleanup

---
+++ Source/New/Models/Slithin.Modules.Repository.Models/DatabaseAccessor.cs
@@ -0,0 +1,18 @@
+﻿using LiteDB;
+
+namespace Slithin.Modules.Repository.Models;
+
+public class DatabaseAccessor
+{
+    public DatabaseAccessor(LiteDatabase dB)
+    {
+        DB = dB;
+    }
+
+    ~DatabaseAccessor()
+    {
+        DB.Dispose();
+    }
+
+    public LiteDatabase DB { get; set; }
+}
