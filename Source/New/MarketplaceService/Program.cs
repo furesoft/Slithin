79b7670344f09022395a2f8ad284ef1ad71ae1a9
filmee24 <filmee24@gmail.com>
2023-01-04 15:43:19 +0100

    Add Core gRPC Service

---
+++ Source/New/MarketplaceService/Program.cs
@@ -0,0 +1,41 @@
+﻿using MarketplaceService.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Logging;
+
+namespace MarketplaceService;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddGrpc();
+        builder.Services.AddGrpcReflection();
+
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(o =>
+        {
+            o.RequireHttpsMetadata = false;
+
+            var validator = new JwtTokenValidator();
+            o.SecurityTokenValidators.Add(validator);
+        });
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+        IdentityModelEventSource.ShowPII = true;
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapGrpcService<Authentication>();
+        app.MapGrpcService<Storage>();
+
+        app.MapGrpcReflectionService();
+
+        app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+        app.Run();
+    }
+}
---
+++ Source/New/MarketplaceService/Services/AuthenticationService.cs
@@ -0,0 +1,29 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Grpc.Core;
+using MarketplaceService.Protos;
+using Microsoft.IdentityModel.Tokens;
+
+namespace MarketplaceService.Services;
+
+public class Authentication : AuthenticationService.AuthenticationServiceBase
+{
+    public override async Task<AuthResult> Authenticate(Grant request, ServerCallContext context)
+    {
+        var claims = new List<Claim>();
+        claims.Add(new Claim(ClaimTypes.Role, "admin"));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Sub, request.Username));
+        claims.Add(new Claim(JwtRegisteredClaimNames.UniqueName, Guid.NewGuid().ToString()));
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("my super secret key"));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var token = new JwtSecurityToken(issuer: "Furesoft", expires: DateTime.Today.AddDays(300),
+            claims: claims, signingCredentials: creds);
+
+        var handler = new JwtSecurityTokenHandler();
+        var encodedToken = handler.WriteToken(token);
+
+        return new AuthResult { Token = encodedToken };
+    }
+}
