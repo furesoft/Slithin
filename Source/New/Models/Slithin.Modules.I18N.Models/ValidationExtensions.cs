b98875f1c50b22dd586315849e66ebd93bdc2e3e
SIGLOCH\c.anders <c.anders@sigloch.de>
2023-01-12 08:37:50 +0100

    Add Extension To Validation To Automaticly Use Translated String

---
+++ Source/New/Models/Slithin.Modules.I18N.Models/ValidationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using AuroraModularis;
+
+namespace Slithin.Modules.I18N.Models;
+
+public static class ValidationExtensions
+{
+    public static IRuleBuilderOptions<T, IList<TElement>> WithLocalisedMessage<T, TElement>(this IRuleBuilder<T, IList<TElement>> ruleBuilder, string key)
+    {
+        var localisatinService = Container.Current.Resolve<ILocalisationService>();
+        
+        return ruleBuilder.WithMessage(localisatinService.GetString(key));
+    }
+}
