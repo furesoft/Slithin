81a5d383041f4beeca6934b68a52e5974feb16a4
SIGLOCH\c.anders <c.anders@sigloch.de>
2023-01-17 09:42:45 +0100

    Add FilterPopup Control

---
+++ Source/New/Modules/Slithin.Modules.Templates.UI/FilterPopup.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Slithin.Modules.Templates.UI;
+
+public partial class FilterPopup : UserControl
+{
+    public FilterPopup()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+
