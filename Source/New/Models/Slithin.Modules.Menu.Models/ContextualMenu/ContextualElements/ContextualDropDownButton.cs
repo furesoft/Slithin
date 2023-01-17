3b267cb2982f747526543a107f50d5e6429a9df6
SIGLOCH\c.anders <c.anders@sigloch.de>
2023-01-17 09:42:12 +0100

    Add ContextualDropDownButton

---
+++ Source/New/Models/Slithin.Modules.Menu.Models/ContextualMenu/ContextualElements/ContextualDropDownButton.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using Avalonia.Controls;
+
+namespace Slithin.Modules.Menu.Models.ContextualMenu.ContextualElements;
+
+public class ContextualDropDownButton : ContextualButton
+{
+    public Control DropDown { get; set; }
+
+    public ContextualDropDownButton(string title, string iconName, Control dropdown) : base(title, iconName, null)
+    {
+        DropDown = dropdown;
+
+        PropertyChanged += OnPropertyChanged;
+    }
+
+    private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(DataContext))
+        {
+            DropDown.DataContext = DataContext;
+        }
+    }
+}
