119f67aa33c58bd6083bc3bd1493f5b0d59daf5f
DasLixou <einlixou@gmail.com>
2023-02-24 14:57:41 +0100

    Synchronizing Notebooks

--- Source/New/Modules/Slithin.Modules.Sync/SynchronizeImpl.cs
+++ Source/New/Modules/Slithin.Modules.Sync/SynchronizeImpl.cs
@@ -1,4 +1,5 @@
 ﻿using AuroraModularis.Core;
+using Slithin.Modules.BaseServices.Models;
 using Slithin.Modules.Device.Models;
 using Slithin.Modules.Sync.Models;
 using Slithin.Modules.UI.Models;
@@ -11,9 +12,39 @@ public class SynchronizeImpl : ISynchronizeService
     {
         await Task.Run(() =>
         {
-            ServiceContainer.Current.Resolve<INotificationService>().ShowStatus("Synchronizing Device");
-            var notebooks = ServiceContainer.Current.Resolve<IRemarkableDevice>().FetchFilesWithModified("/home/root/.local/share/remarkable/xochitl");
-            var a = 1;
+            var device = ServiceContainer.Current.Resolve<IRemarkableDevice>();
+            var pathManager = ServiceContainer.Current.Resolve<IPathManager>();
+            var notificationService = ServiceContainer.Current.Resolve<INotificationService>();
+
+            {
+                var status = notificationService.ShowStatus("Synchronizing Device: Fetching Notebooks");
+                var notebooks = device.FetchFilesWithModified("/home/root/.local/share/remarkable/xochitl");
+
+                foreach (var (p, time) in notebooks)
+                {
+                    var path = Path.Combine(pathManager.NotebooksDir, p);
+                    var fileInfo = new FileInfo(path);
+                    if (!fileInfo.Exists || IsFileOlder(fileInfo, time))
+                    {
+                        status.Step($"Sync Notebook: {p} [Downloading...]");
+                        device.Download($"/home/root/.local/share/remarkable/xochitl/{p}", fileInfo);
+                    }
+                    else
+                    {
+                        status.Step($"Sync Notebook: {p} [Up to date]");
+                    }
+                }
+            }
+
+            notificationService.ShowStatus("Synchronizing Device: Fetching Templates");
+
+            var templates = device.FetchFilesWithModified("/usr/share/remarkable/templates");
         });
+    }
+
+    private static bool IsFileOlder(FileInfo file, long other)
+    {
+        var time = file.LastWriteTime.Ticks;
+        return time < other;
     }
 }
