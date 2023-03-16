a394a727189bf7c4492bc31c3d83ab809e42cce0
Chris <filmee24@gmail.com>
2023-03-16 20:27:15 +0100

    refactor: Cleanup

--- Source/Slithin.Controls/Theme/Styles/RadioButtonGroup.axaml.cs
+++ Source/Slithin.Controls/Theme/Styles/RadioButtonGroup.axaml.cs
@@ -3,38 +3,37 @@ using Avalonia.Controls;
 using Avalonia.Layout;
 using Avalonia.LogicalTree;
 
-namespace Slithin.Controls.Theme.Styles
+namespace Slithin.Controls.Theme.Styles;
+
+public class RadioButtonGroup : ItemsControl
 {
-    public class RadioButtonGroup : ItemsControl
+    public static readonly StyledProperty<string> GroupNameProperty =
+        AvaloniaProperty.Register<RadioButtonGroup, string>(nameof(GroupName));
+
+    public static readonly StyledProperty<Orientation> OrientationProperty =
+        AvaloniaProperty.Register<RadioButtonGroup, Orientation>(nameof(Orientation), Orientation.Horizontal);
+
+    public string GroupName
     {
-        public static readonly StyledProperty<string> GroupNameProperty =
-            AvaloniaProperty.Register<RadioButtonGroup, string>(nameof(GroupName));
+        get => GetValue(GroupNameProperty);
+        set => SetValue(GroupNameProperty, value);
+    }
 
-        public static readonly StyledProperty<Orientation> OrientationProperty =
-            AvaloniaProperty.Register<RadioButtonGroup, Orientation>(nameof(Orientation), Orientation.Horizontal);
+    public Orientation Orientation
+    {
+        get => GetValue(OrientationProperty);
+        set => SetValue(OrientationProperty, value);
+    }
 
-        public string GroupName
+    protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToLogicalTree(e);
+
+        foreach (var item in Items)
         {
-            get => GetValue(GroupNameProperty);
-            set => SetValue(GroupNameProperty, value);
-        }
-
-        public Orientation Orientation
-        {
-            get => GetValue(OrientationProperty);
-            set => SetValue(OrientationProperty, value);
-        }
-
-        protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e)
-        {
-            base.OnAttachedToLogicalTree(e);
-
-            foreach (var item in Items)
+            if (item is RadioButton radioButton)
             {
-                if (item is RadioButton radioButton)
-                {
-                    radioButton.GroupName = GroupName;
-                }
+                radioButton.GroupName = GroupName;
             }
         }
     }
