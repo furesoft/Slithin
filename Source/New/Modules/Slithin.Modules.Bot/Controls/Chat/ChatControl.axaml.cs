c8a694a3f37ab00316a2491c8cf00915c8f28421
Chris <filmee24@gmail.com>
2023-05-20 11:43:16 +0200

    fix: Chatbot Scrolling

--- Source/New/Modules/Slithin.Modules.Bot/Controls/Chat/ChatControl.axaml.cs
+++ Source/New/Modules/Slithin.Modules.Bot/Controls/Chat/ChatControl.axaml.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.Timers;
 using Avalonia.Controls;
 using Avalonia.Controls.Generators;
 using Avalonia.Controls.Presenters;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using Timer = System.Timers.Timer;
 
 namespace Slithin.Modules.Bot.Controls.Chat;
 
 public partial class ChatControl : UserControl
 {
+    private Timer _timer;
+
     public ChatControl()
     {
         InitializeComponent();
@@ -17,19 +22,39 @@ public partial class ChatControl : UserControl
     protected override void OnInitialized()
     {
         base.OnInitialized();
-        
-        ((ObservableCollection<ChatMessage>)this.FindControl<ItemsPresenter>("chatList").Items).CollectionChanged += OnCollectionChanged;
-        this.FindControl<ItemsPresenter>("chatList").ItemContainerGenerator.Materialized += ItemContainerGeneratorOnMaterialized;
+
+        ((ObservableCollection<ChatMessage>)this.FindControl<ItemsPresenter>("chatList").Items).CollectionChanged +=
+            OnCollectionChanged;
+        this.FindControl<ItemsPresenter>("chatList").ItemContainerGenerator.Materialized +=
+            ItemContainerGeneratorOnMaterialized;
+
+        _timer = new System.Timers.Timer();
+        _timer.Interval = 1000;
+        _timer.AutoReset = false;
+        _timer.Elapsed += TimerOnElapsed;
     }
 
     private void OnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
     {
-        this.FindControl<ScrollViewer>("chatScrollViewer").ScrollToEnd();
+        ScrollToEnd();
     }
 
     private void ItemContainerGeneratorOnMaterialized(object? sender, ItemContainerEventArgs e)
     {
-       this.FindControl<ScrollViewer>("chatScrollViewer").ScrollToEnd();
+        ScrollToEnd();
+    }
+
+    private void ScrollToEnd()
+    {
+        _timer.Start();
+    }
+
+    private void TimerOnElapsed(object? sender, ElapsedEventArgs e)
+    {
+        Dispatcher.UIThread.InvokeAsync(() =>
+        {
+            this.FindControl<ScrollViewer>("chatScrollViewer").ScrollToEnd();
+        });
     }
 
     private void InitializeComponent()
